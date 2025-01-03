fe5b3f5ea88ffaf9d9aaeb43d867626de1c55b16
Lucas Müllner <lucasmuellner@gmail.com>
2025-01-03 14:13:49 +0100

    implements chapter 11 exercises

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"strings"
+)
 
+//go:embed rights
+var unRights embed.FS
+
+func main() {
+	if len(os.Args) == 1 {
+		printFiles()
+		os.Exit(0)
+	}
+
+	fileName := "UN-" + os.Args[1] + "-rights.txt"
+
+	data, err := os.ReadFile("rights/" + fileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(data))
+}
+
+func printFiles() {
+	fmt.Println("contents:")
+	err := fs.WalkDir(unRights, "rights",
+		func(path string, d fs.DirEntry, err error) error {
+			if !d.IsDir() {
+				_, fileName, _ := strings.Cut(path, "/")
+				fmt.Println(fileName)
+			}
+			return nil
+		})
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
